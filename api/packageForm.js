e5bfdc9be83864e79dbe10b157861c5e810f3cbc
webvertize-ro <contact@webvertize.ro>
2026-03-26 10:34:17 +0200

    further changes

--- api/packageForm.js
+++ api/packageForm.js
@@ -1,7 +1,25 @@
 export default async function handler(req, res) {
-  try {
-    console.log('req.body is: ', req.body);
-  } catch (error) {
-    console.error(error.message);
+  if (req.method !== 'POST') {
+    return res.status(400).json({ status: 'Method not allowed!' });
   }
+
+  const { name, phone, email, message, cf_turnstile_token } = req.body;
+
+  // 1. Insert in the "submissions" table
+  // 2. Send the email with collected data
+
+  // Turnstile validation
+  const responseToken = await fetch(
+    'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        secret: process.env.TURNSTILE_SECRET,
+        response: cf_turnstile_token,
+      }),
+    },
+  ).then((res) => res.json());
+
+  console.log('responseToken: ', responseToken);
 }
