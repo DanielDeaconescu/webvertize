29ee14aeb3a69eff4816bc2b9119009ff52e76bd
webvertize-ro <contact@webvertize.ro>
2026-03-27 16:32:55 +0200

    further changes

--- src/components/CookiePopupLanding.jsx
+++ src/components/CookiePopupLanding.jsx
@@ -3,94 +3,44 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledCookiePopup = styled.div`
+const CookiePopup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   bottom: 1rem;
-  left: 0;
-  right: 0;
-  width: 80%;
-  z-index: 100;
-  background-color: #1b3c53;
-  color: #fff;
-  border-top: 3px solid #fff;
-  padding: 1rem;
+  background-color: grey;
   justify-self: center;
-  border-radius: 2rem;
-  /* Glassmorphism effect */
-  background: rgba(27, 60, 83, 0.65);
-  border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5px);
-  -webkit-backdrop-filter: blur(5px);
-  border: 1px solid rgba(255, 255, 255, 0.3);
+  padding: 1rem;
+  border-radius: 0.75rem;
 `;
 
-const CookiePopupInner = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
+const StyledP = styled.p`
+  margin: 0;
 `;
 
-const Row = styled.div`
+const ContainerButtons = styled.div`
   display: flex;
-  align-items: center;
-`;
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  gap: 0.5rem;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    flex-direction: row;
-    width: 100%;
-  }
+  gap: 0.25rem;
 `;
 
 const StyledButton = styled.button`
   border: none;
-  padding: 0.25rem 1rem;
-  border-radius: 10px;
-
-  @media (max-width: 768px) {
-    font-size: 0.9rem;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    flex: 1;
-  }
+  border-radius: 0.25rem;
+  background-color: #1b3c53;
+  color: #fff;
+  padding: 0.5rem;
 `;
 
 const StyledLink = styled(Link)`
+  text-align: center;
   text-decoration: none;
-  color: #000;
-  background-color: #fff;
-  padding: 0.25rem 1rem;
-  border-radius: 10px;
-
-  @media (max-width: 768px) {
-    font-size: 0.9rem;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    flex: 6;
-    display: flex;
-    justify-content: center;
-  }
-`;
-
-const StyledP = styled.p`
-  display: flex;
-  text-align: justify;
-  margin: 0;
-
-  @media (max-width: 768px) {
-    font-size: 0.9rem;
-  }
+  border: none;
+  border-radius: 0.25rem;
+  background-color: #1b3c53;
+  color: #fff;
+  padding: 0.5rem;
 `;
 
 function CookiePopupLanding() {
@@ -105,34 +55,26 @@ function CookiePopupLanding() {
   }
 
   return (
-    <>
+    <div>
       {!acceptedCookies && (
-        <StyledCookiePopup>
-          <CookiePopupInner className="container">
-            <Row className="row">
-              {/* Message */}
-              <div className="col-md-8 mb-3">
-                <StyledP>
-                  Webvertize nu folosește cookies momentan. Vă vom anunța dacă
-                  le implementăm.
-                </StyledP>
-              </div>
-              {/* Buttons */}
-              <div className="col-md-4 d-flex align-items-center justify-content-center">
-                <ButtonsContainer>
-                  <StyledButton onClick={() => handleAcceptedCookies()}>
-                    Ok
-                  </StyledButton>
-                  <StyledLink to="/cookies">
-                    Mai multe despre cookies
-                  </StyledLink>
-                </ButtonsContainer>
-              </div>
-            </Row>
-          </CookiePopupInner>
-        </StyledCookiePopup>
+        <CookiePopup className="container">
+          {/* Text */}
+          <div>
+            <StyledP>
+              Webvertize nu folosește cookies momentan. Vă vom anunța dacă le
+              implementăm.
+            </StyledP>
+          </div>
+          {/* Container for buttons */}
+          <ContainerButtons>
+            <StyledButton onClick={() => handleAcceptedCookies()}>
+              Ok
+            </StyledButton>
+            <StyledLink>Mai multe despre cookies</StyledLink>
+          </ContainerButtons>
+        </CookiePopup>
       )}
-    </>
+    </div>
   );
 }
 
