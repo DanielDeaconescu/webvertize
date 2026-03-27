7ea3935a6d5c9ac7ff99ec3eeadfe2721ef1225e
webvertize-ro <contact@webvertize.ro>
2026-03-27 16:33:35 +0200

    further changes

--- src/components/CookiePopupLanding.jsx
+++ src/components/CookiePopupLanding.jsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 const CookiePopup = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 0.35rem;
   justify-content: center;
   align-items: center;
   position: fixed;
