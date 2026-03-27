6038db69378dd376c06957d0e55072315b078888
webvertize-ro <contact@webvertize.ro>
2026-03-27 16:37:11 +0200

    further changes

--- src/components/CookiePopupLanding.jsx
+++ src/components/CookiePopupLanding.jsx
@@ -10,11 +10,21 @@ const CookiePopup = styled.div`
   justify-content: center;
   align-items: center;
   position: fixed;
+  z-index: 999;
+  width: 80%;
   bottom: 1rem;
-  background-color: grey;
+
   justify-self: center;
   padding: 1rem;
   border-radius: 0.75rem;
+
+  /* glassmorphism effect */
+  background: rgb(27, 60, 83, 0.75);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
 `;
 
 const StyledP = styled.p`
@@ -29,7 +39,7 @@ const ContainerButtons = styled.div`
 const StyledButton = styled.button`
   border: none;
   border-radius: 0.25rem;
-  background-color: #1b3c53;
+  background-color: #1f465f;
   color: #fff;
   padding: 0.5rem;
 `;
@@ -39,7 +49,7 @@ const StyledLink = styled(Link)`
   text-decoration: none;
   border: none;
   border-radius: 0.25rem;
-  background-color: #1b3c53;
+  background-color: #1f465f;
   color: #fff;
   padding: 0.5rem;
 `;
