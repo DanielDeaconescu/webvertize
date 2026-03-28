5baf87d98b87d906020cd14551184a6bfab1495a
webvertize-ro <contact@webvertize.ro>
2026-03-28 10:19:28 +0200

    further changes

--- src/pages/PackagesPage.jsx
+++ src/pages/PackagesPage.jsx
@@ -7,6 +7,8 @@ import CookiePopup from '../components/Cookies/CookiePopup';
 import Footer from '../components/Footer';
 import Packages from '../components/Packages';
 import NextSteps from '../components/NextSteps';
+import Header from '../components/Header';
+import pricesHeaderImg from '../assets/prices_header_img.jpg';
 
 const StyledPackagesPage = styled.div`
   background-color: #d3d3d3;
@@ -17,6 +19,12 @@ function LandingPage() {
   return (
     <StyledPackagesPage>
       <Navigation />
+      <Header
+        bgImage={pricesHeaderImg}
+        title="header.prices.title"
+        text1="header.prices.text1"
+        text2="header.prices.text2"
+      />
       <SideButtons />
       <Packages />
       <NextSteps />
